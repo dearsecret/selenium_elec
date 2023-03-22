836802a4218e19dc92c98557be8d1a6e6b284f65
dearsecret <dearsecret1217@gmail.com>
2023-03-22 14:56:46 +0900

    pandas dataframe

--- hanjeon.py
+++ hanjeon.py
@@ -1,5 +1,6 @@
 import time
 import os
+import pandas as pd
 from dotenv import load_dotenv
 
 from datetime import datetime
@@ -9,11 +10,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 
+
 load_dotenv()
 HANJEON_ID = os.getenv("HANJEON_ID")
 HANJEON_PW = os.getenv("HANJEON_PW")
 
-user_lst_path = os.path.abspath(os.path.join(os.getcwd(), "cust_lst"))
+user_lst_path = os.path.abspath(os.path.join(os.getcwd(), "cust_lst.txt"))
 f = open(user_lst_path, "r")
 lines = f.readlines()
 user_lst = [line.strip() for line in lines]
@@ -23,6 +25,20 @@ browser = webdriver.Chrome()
 browser.get(
     "https://cyber.kepco.co.kr/ckepco/front/jsp/ME/C/A/MECALP001_elec.jsp?login_type=1"
 )
+
+
+# def wait_and_clear(driver, element, timeout=5, n=0):
+#     """wait for the element and clear the content"""
+#     try:
+#         WebDriverWait(driver, timeout).until(
+#             EC.presence_of_element_located((By.XPATH, element))
+#         )
+#     except:
+#         wait_and_clear(driver, element, timeout)
+
+#     finally:
+#         n += 1
+#         print(f"{n}번 시도")
 
 
 ## 로그인
@@ -63,7 +79,6 @@ for cust in user_lst:
 
     cust_id = browser.find_element(By.XPATH, '//*[@id="searchCustNo"]/dd/input')
     cust_id.send_keys(cust)
-    new_dict = {}
     search_button = browser.find_element(
         By.XPATH, '//*[@id="content"]/div[3]/form/fieldset/div[2]/a'
     )
@@ -84,15 +99,14 @@ for cust in user_lst:
         By.XPATH, '//*[@id="content"]/div[4]/div[1]/table/tbody/tr/td[6]/div'
     ).text
 
-    new_dict["bill_date"] = bill_date
-    new_dict["kwh"] = kwh
-    new_dict["price"] = price
+    total[cust] = [bill_date, kwh, price]
 
-    total[cust] = new_dict
     time.sleep(1)
     browser.find_element(By.XPATH, '//*[@id="searchCustNo"]/dd/input').clear()
 
-print(total)
+df = pd.DataFrame.from_dict(total, orient="index", columns=["날짜", "kwh", "청구요금"])
+df.to_csv("result.csv")
+print(df)
 # now = datetime.now()
 # with open() as f:
 #   f.write("")
